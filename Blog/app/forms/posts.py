0c96431627aabca32116c618e3999691330275eb
YY <43437773+YYAXAY@users.noreply.github.com>
2020-04-04 17:37:55 +0800

    Update posts.py

--- Blog/app/forms/posts.py
+++ Blog/app/forms/posts.py
@@ -6,7 +6,7 @@ from flask_pagedown.fields import PageDownField
 
 class PostsForm(FlaskForm):
     # PageDownField提供预览功能，如果想要设置字段的其它属性，可以通过render_kw完成
-    content = PageDownField('今天想写点什么......', render_kw={'placeholder': '注意输入框下的格式预览'}, validators=[DataRequired(), Length(1, 5000, message='评论字数不能超过5000')])
+    content = PageDownField('今天想写点什么......', render_kw={'placeholder': '注意输入框下的格式预览，每行开头四个空格进入富文本编辑模式'}, validators=[DataRequired(), Length(1, 5000, message='评论字数不能超过5000')])
     submit = SubmitField('发表')
 
 
